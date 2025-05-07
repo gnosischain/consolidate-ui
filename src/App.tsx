7237da338fa8c1011f1fb426574a0054ea96ab27
Wagalidoom <lukas-8@live.fr>
2025-05-07 15:22:54 +0200

    feat: add disclaimer banner

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { SelectNetwork } from './components/SelectNetwork';
 import { truncateAddress } from './utils/address';
 import Consolidate from './components/Consolidate';
 import useContractConfig from './hooks/useContractConfig';
+import { DisclaimerBanner } from './components/Disclaimer';
 
 function App() {
 	const { disconnect } = useDisconnect();
@@ -18,29 +19,31 @@ function App() {
 		<div className="w-full h-screen flex flex-col">
 			<div className="navbar bg-base-200 shadow-sm">
 				<div className="flex-1">
-				<div className='flex justify-center sm:justify-start mt-4 sm:mt-0'>
-                <img
-                  src='/logo.svg'
-                  alt='Gnosis Logo'
-                  width={45}
-                  height={24}
-                />
-                <div className='flex flex-col ml-2 justify-center'>
-                  <img
-                    src='/gnosis.svg'
-                    alt='Gnosis Text'
-                    width={100}
-                    height={24}
-                    className='mb-1 mt-0.5'
-                  />
-                  <p className='text-[6px] leading-[6px] sm:text-[8px] sm:leading-[8px] mt-1 '>
-                    BEACON CHAIN CONSOLIDATION
-                  </p>
-				  </div>
-                </div>
+					<div className="flex justify-center sm:justify-start mt-4 sm:mt-0">
+						<img src="/logo.svg" alt="Gnosis Logo" width={45} height={24} />
+						<div className="flex flex-col ml-2 justify-center">
+							<img
+								src="/gnosis.svg"
+								alt="Gnosis Text"
+								width={100}
+								height={24}
+								className="mb-1 mt-0.5"
+							/>
+							<p className="text-[6px] leading-[6px] sm:text-[8px] sm:leading-[8px] mt-1 ">
+								BEACON CHAIN CONSOLIDATION
+							</p>
+						</div>
+					</div>
 				</div>
 				<div className="flex gap-x-2">
-					<a href="https://www.gnosis.io/" target='_blank' className="underline text-sm flex items-center">Explore<img src='/external.svg' alt='External Link' className='w-3 h-3 ml-1'/></a>
+					<a
+						href="https://www.gnosis.io/"
+						target="_blank"
+						className="underline text-sm flex items-center"
+					>
+						Explore
+						<img src="/external.svg" alt="External Link" className="w-3 h-3 ml-1" />
+					</a>
 					{account.isConnected && account.address && chainId ? (
 						<div className="flex items-center gap-x-2">
 							<p className="text-sm">{truncateAddress(account.address)}</p>
@@ -64,6 +67,7 @@ function App() {
 					)}
 				</div>
 			</div>
+			<DisclaimerBanner />
 		</div>
 	);
 }
