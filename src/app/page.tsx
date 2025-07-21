7a1ddcbe0a9df8c1fc81b37911b6819b8b8b1bf5
Wagalidoom <lukas-8@live.fr>
2025-07-21 20:26:18 +0200

    fix: default to dark theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ import { useWallet } from '../context/WalletContext';
 import Navbar from '../components/Navbar';
 import { Balance } from '../components/Balance';
 
-function App() {	
+function App() {
 	const { account, chainId, network, isWrongNetwork, balance } = useWallet();
 
 	if (isWrongNetwork) {
@@ -18,17 +18,15 @@ function App() {
 		<div className="w-full h-screen flex flex-col">
 			<Navbar />
 			{/* Main content */}
-			<div className="flex h-full w-full items-center justify-center bg-base-100">
-				<div className="flex flex-col gap-y-4 max-w-4xl w-full bg-slate-800 rounded-lg shadow-sm p-8">
-					{account.isConnected && account.address && network && chainId && balance ? (
-						<>
-							<Balance />
-							<Consolidate />
-						</>
-					) : (
-						<h1 className="text-xl font-bold">Please connect your wallet to continue</h1>
-					)}
-				</div>
+			<div className="flex px-8 flex-col h-full w-full items-center justify-center bg-base-100">
+				{account.isConnected && account.address && network && chainId && balance ? (
+					<>
+						<Balance />
+						<Consolidate />
+					</>
+				) : (
+					<h1 className="text-xl font-bold">Please connect your wallet to continue</h1>
+				)}
 			</div>
 			<DisclaimerBanner />
 		</div>
