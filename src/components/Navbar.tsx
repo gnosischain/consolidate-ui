67718be4ccf4cb8286f4608c27a01e45eda82822
Wagalidoom <lukas-8@live.fr>
2025-09-19 11:13:40 +0200

    feat: add gnosis theme

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,20 +5,8 @@ export default function Navbar() {
 		<div className="navbar z-50 bg-base-200 shadow-sm">
 			<div className="flex-1">
 				<div className="flex justify-center sm:justify-start mt-4 sm:mt-0">
-					<img src="/logo.svg" alt="Gnosis Logo" width={45} height={24} />
-					<div className="flex flex-col ml-2 justify-center">
-						<img
-							src="/gnosis.svg"
-							alt="Gnosis Text"
-							width={100}
-							height={24}
-							className="mb-1 mt-0.5"
-						/>
-						<p className="text-[6px] leading-[6px] sm:text-[8px] sm:leading-[8px] mt-1 ">
-							BEACON CHAIN CONSOLIDATION
-						</p>
-					</div>
-					<p className="text-sm font-semibold text-red-500">
+					Gnosis Launchpad
+					<p className="text-sm font-semibold text-red-500 ml-2">
 						BETA
 					</p>
 				</div>
