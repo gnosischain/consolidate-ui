ae8ece8f4e07631431ed4983675ed52a445d9cfd
Wagalidoom <lukas-8@live.fr>
2025-11-10 06:48:19 +0100

    feat: add statistics hook

--- src/app/explorer/[validatorIndex]/page.tsx
+++ src/app/explorer/[validatorIndex]/page.tsx
@@ -1,13 +1,22 @@
+'use client'; //TODO fetch directly without hook
+
+import { StatisticsTable } from "../../../components/StatisticsTable";
+import { useStatistics } from "../../../hooks/useStatistics";
+
 export default async function Page({
     params,
 }: {
     params: Promise<{ validatorIndex: string }>;
 }) {
     const { validatorIndex } = await params;
+    const { statistics } = useStatistics(validatorIndex);
+
+    if (!statistics) { return (<div>No statistics found</div>) }
 
     return (
         <div className="w-full flex items-center justify-center text-gray-500">
             Validator {validatorIndex}
+            <StatisticsTable statistics={statistics} />
         </div>
     );
 }
---
+++ src/types/statistics.ts
@@ -0,0 +1,6 @@
+export interface Statistics {
+    last_24h: bigint;
+    last_7d: bigint;
+    last_30d: bigint;
+    all: bigint;
+}
