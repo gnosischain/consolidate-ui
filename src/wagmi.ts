d7b79d33d63cd5edb1cf971588b1cb59d9cd72df
Wagalidoom <lukas-8@live.fr>
2026-01-03 14:22:12 +0100

    feat: improve select wallet modal

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -1,10 +1,9 @@
 import { http, createConfig } from 'wagmi';
 import { gnosis, gnosisChiado } from 'wagmi/chains';
-import { safe } from 'wagmi/connectors';
 
 export const config = createConfig({
 	chains: [gnosis, gnosisChiado],
-	connectors: [safe()],
+	connectors: [],
 	transports: {
 		// [mainnet.id]: http(),
 		// [sepolia.id]: http(),
