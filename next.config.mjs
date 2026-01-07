8675dd98ccee3f6d28a5f5a0e48c6b87cb00e2f0
Wagalidoom <lukas-8@live.fr>
2026-01-07 18:03:39 +0100

    feat: update csp

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,32 @@
 /** @type {import('next').NextConfig} */
+
+const cspHeader = `
+    default-src 'self';
+    script-src 'self' 'unsafe-eval' 'unsafe-inline';
+    style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
+    img-src 'self' blob: data:;
+    font-src 'self' https://fonts.gstatic.com;
+    connect-src 'self' https://rpc.gnosischain.com/ https://dune-proxy.gnosischain.com/ https://rpc.chiadochain.net/ https://indexer.hyperindex.xyz/;
+	frame-ancestors https://app.safe.global;
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    upgrade-insecure-requests;
+`
+
 const nextConfig = {
 	async headers() {
 		return [
+			{
+				source: '/(.*)',
+				headers: [
+				  {
+					key: 'Content-Security-Policy',
+					value: cspHeader.replace(/\n/g, ''),
+				  },
+				],
+			  },
 			{
 				source: '/manifest\\.json',
 				headers: [
